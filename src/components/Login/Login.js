d991cc5b66731c645154e171d362c15e942be7e4
youssef-meleka <youssefatef00@gmail.com>
2024-03-16 14:37:13 +0200

    useEffect applied

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,33 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  //the whole purpose of this is to use user's input debouncing, so we don't check 'setFormIsValid' everytime
+  //user presses a keystroke, which is a huge load, which is solved here better where the 'identifier' is 
+  //identifier: is an identifier for the timer that was set which you can use this identifier 
+  //to clear this timer using 'clearTimeout', so that make sure that whenever the cleanup function runs
+  //I clear the timer that was set before this cleanup function ran 
+  useEffect(()=>{
+    const identifier = setTimeout(()=>{
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },500);
+
+    // this runs as a cleanup process (function) before useEffect executes its function the next time,
+    //so before the useEffect function run -except for the very first time- this cleanup function runs
+    return ()=> {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
+    
+  },[enteredEmail, enteredPassword]);
+  
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
