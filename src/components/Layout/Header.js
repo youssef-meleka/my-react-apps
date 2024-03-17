618a05ff5ffd1f35cc2aa497adfff016c876ab8f
youssef-meleka <youssefatef00@gmail.com>
2024-03-17 22:00:51 +0200

    header created

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import mealsImage from '../../components/assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>My Meals App</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt= 'A table full of delicious food!' />
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
